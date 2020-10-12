5b12fdcc9e071a70a438cc73c10e90eaca27484f
Okechukwu O Uneze <unezeo@gmail.com>
2020-10-12 19:36:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // add solution here
-function theBeatlesPlay(a,b) {
-  var musicians = [][];
+function theBeatlesPlay(a , b) {
+  var musicians = [];
   for (var i = 0; i < a.length; i++) {
     musicians [i] = a [i];
   }
+  var instruments = [];
   for (var i = 0; i < b.length; i++) {
     instruments [i] = a [i];
   }
