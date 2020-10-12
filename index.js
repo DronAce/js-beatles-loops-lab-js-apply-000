634ac2fb77b0e7d563110d3f423eee6aa9dae950
Okechukwu O Uneze <unezeo@gmail.com>
2020-10-12 19:23:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 function theBeatlesPlay(a , b) {
   var musicians = [];
   for (int i = 0; i < a.length; i+++) {
-    musicians [i] = a [i]
+    musicians [i] = a [i];
   }
   var instruments = [];
   for (int i = 0; i < b.length; i+++) {
-    instruments [i] = a [i]
+    instruments [i] = a [i];
   }
 }
 
